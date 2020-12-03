2cc7ff6810cffb5b01204d892d856ad8a6676d97
Lucas <lucas.gautier@live.fr>
2020-12-03 20:15:28 +0100

    homepage

--- app/javascript/packs/src/pages/Home.jsx
+++ app/javascript/packs/src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import React from "react"
 //import CourseForm from "../components/course/CourseForm"
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
 import Registration from "../components/Registration";
 import CourseIndex from "../components/course/CourseIndex"
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Bienvenue sur la Homepage</h1>
-      <CourseIndex />
-      <Registration />
-    </div>
+      <div>
+        <h1 className="welcome">Bienvenue sur la Homepage</h1>
+        <div className="search-bar">
+          <h1> Je recherche </h1>
+            <div className="category"> insert filters</div>
+        </div>
+        <CourseIndex />
+        <Registration />
+      </div>
   )
 }
 
